bab5e2c2434c79325bc93edafdcc7aa051dab110
Lalit Kumar <lalit.k.saroya@gmail.com>
2024-03-10 22:25:57 -0600

    Driver Source File
    File Created

---
+++ LIS3MDLTR_I2C/LIS3MDL_I2C.c
@@ -0,0 +1,23 @@
+/*
+ * 									LIS3MDL_I2C.c
+ *
+ *  This file contains LIS3MDL driver API implementations for I2C communication.
+ *
+ */
+
+#include "LIS3MDL_I2C.h"
+
+
+/* ------------------------------------------------------------------------------------------------------
+ * Name		:	LIS3MDL_DefaultInit
+ * Description	:	Initialize LISM3MDL in default configuration
+ * Parameter 1	:	Pointer to I2C Handle
+ * Return Type	:	none (void)
+ * Note		:	In this initialization:
+ * 				- Temperature sensor is enabled
+ * 				-
+ * ------------------------------------------------------------------------------------------------------ */
+void LIS3MDL_DefaultInit(I2C_HandleTypeDef *pI2CHandle)
+{
+
+}
