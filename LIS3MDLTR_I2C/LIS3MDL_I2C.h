2c3d26808b337f8fb94046e5c01184144c73ba7f
Lalit Kumar <lalit.k.saroya@gmail.com>
2024-03-12 20:09:02 -0600

    Update LIS3MDL_I2C.h

--- LIS3MDLTR_I2C/LIS3MDL_I2C.h
+++ LIS3MDLTR_I2C/LIS3MDL_I2C.h
@@ -22,35 +22,33 @@
 #define CTRL_REG_4	0x23
 #define CTRL_REG_5	0x24
 #define STATUS_REG	0x27
-#define OUT_X_L	0x28
-#define OUT_X_H	0x29
-#define OUT_Y_L	0x2A
-#define OUT_Y_H	0x2B
-#define OUT_Z_L	0x2C
-#define OUT_Z_H	0x2D
+#define OUT_X_L    0x28
+#define OUT_X_H    0x29
+#define OUT_Y_L    0x2A
+#define OUT_Y_H    0x2B
+#define OUT_Z_L    0x2C
+#define OUT_Z_H    0x2D
 #define TEMP_OUT_L	0x2E
 #define TEMP_OUT_H	0x2F
-#define INT_CFG	0x30
-#define INT_SRC	0x31
+#define INT_CFG    0x30
+#define INT_SRC    0x31
 #define INT_THS_L	0x32
 #define INT_THS_H	0x33
 
 /* LIS3DML APIs*/
 // Initialization
-void LIS3MDL_UserInit(void);
+void LIS3MDL_UserInit(I2C_HandleTypeDef *pI2CHandle);
 void LIS3MDL_DefaultInit(I2C_HandleTypeDef *pI2CHandle);
 // DeInit
-void LIS3MDL_DeInit(void);
-void LIS3MDL_SoftReset(void);
+void LIS3MDL_DeInit(I2C_HandleTypeDef *pI2CHandle);
 
 // Get Sensor Values
-int16_t LIS3MDL_getRAWTemperature(void);
-int16_t LIS3MDL_getTemperature_C(int8_t offset);
+int16_t LIS3MDL_getTemperature_C(I2C_HandleTypeDef *pI2CHandle);
 
-void LIS3MDL_getMAG_XYZ(int16_t *pMagXYZ);
-int16_t LIS3MDL_getMAG_X(void);
-int16_t LIS3MDL_getMAG_Y(void);
-int16_t LIS3MDL_getMAG_Z(void);
+void LIS3MDL_getMAG_XYZ(I2C_HandleTypeDef *pI2CHandle, int16_t *pMagXYZ); // check argument
+int16_t LIS3MDL_getMAG_X(I2C_HandleTypeDef *pI2CHandle);
+int16_t LIS3MDL_getMAG_Y(I2C_HandleTypeDef *pI2CHandle);
+int16_t LIS3MDL_getMAG_Z(I2C_HandleTypeDef *pI2CHandle);
 
 
 
