743b4e0a6419ae2897065d1b172b5dbf9d73da2e
Lalit Kumar <lalit.k.saroya@gmail.com>
2024-03-14 22:41:09 -0600

    Update LIS3MDL_I2C.c

--- LIS3MDLTR_I2C/LIS3MDL_I2C.c
+++ LIS3MDLTR_I2C/LIS3MDL_I2C.c
@@ -70,7 +70,7 @@ void LIS3MDL_DefaultInit(I2C_HandleTypeDef *pI2CHandle)
 	/*- Clearing entire register  -*/
 	data &= (uint8_t)~(0xFF);
 	/*- Bits[7,6,5,4,0] must be 0 -*/
-	/*- Ultra-high performance operation mode for X axis, setting bits[3:2]  -*/
+	/*- Ultra-high performance operation mode for Z axis, setting bits[3:2]  -*/
 	data |= (3<<2);
 	/*- Configure data LSB at lower address, clearing bit[1]  -*/
 	/*- Configuring Register 4 with above settings -*/
@@ -92,13 +92,164 @@ void LIS3MDL_DefaultInit(I2C_HandleTypeDef *pI2CHandle)
  * Name		:	LIS3MDL_UserInit
  * Description	:	Initialize LISM3MDL with user defined configuration
  * Parameter 1	:	Pointer to I2C Handle
- * Parameter 2	:
- * Parameter 3	:
+ * Parameter 2	:	Temperature Sensor Enable or Disable
+ * Parameter 3	:	Output Data Rate
+ * Parameter 4	:	X, Y and Z axis Operating Mode
+ * Parameter 5	:	Full-Scale Selection
+ * Parameter 6	:	Measurement Mode
  * Return Type	:	none (void)
  * Note		:
  * ------------------------------------------------------------------------------------------------------ */
-void LIS3MDL_UserInit(I2C_HandleTypeDef *pI2CHandle)
+void LIS3MDL_UserInit(I2C_HandleTypeDef *pI2CHandle, uint8_t temp_EnorDi, int8_t ODR, int8_t xyzOM, int8_t FScale, int8_t measurementMode)
 {
+	/*- temporary variables to store registers contents  -*/
+	uint8_t reg1Config = 0;
+	uint8_t reg2Config = 0;
+	uint8_t reg3Config = 0;
+	uint8_t reg4Config = 0;
+	uint8_t reg5Config = 0;
+
+	HAL_I2C_Mem_Read(pI2CHandle, MAG_ADDRESS, CTRL_REG_1, I2C_MEMADD_SIZE_8BIT, &reg1Config, 1, HAL_MAX_DELAY);
+	HAL_I2C_Mem_Read(pI2CHandle, MAG_ADDRESS, CTRL_REG_2, I2C_MEMADD_SIZE_8BIT, &reg2Config, 1, HAL_MAX_DELAY);
+	HAL_I2C_Mem_Read(pI2CHandle, MAG_ADDRESS, CTRL_REG_3, I2C_MEMADD_SIZE_8BIT, &reg3Config, 1, HAL_MAX_DELAY);
+	HAL_I2C_Mem_Read(pI2CHandle, MAG_ADDRESS, CTRL_REG_4, I2C_MEMADD_SIZE_8BIT, &reg4Config, 1, HAL_MAX_DELAY);
+	HAL_I2C_Mem_Read(pI2CHandle, MAG_ADDRESS, CTRL_REG_5, I2C_MEMADD_SIZE_8BIT, &reg5Config, 1, HAL_MAX_DELAY);
+
+	reg1Config &= (uint8_t)~(0xFF);
+	reg2Config &= (uint8_t)~(0xFF);
+	reg3Config &= (uint8_t)~(0xFF);
+	reg4Config &= (uint8_t)~(0xFF);
+	reg5Config &= (uint8_t)~(0xFF);
+
+	/*- Temperature sensor Configuration  -*/
+	if (temp_EnorDi)
+	{
+		/*- Enable temperature sensor, setting bit[7]  -*/
+		reg1Config |= (1<<7);
+	}
+
+	/*- ODR Configuration  -*/
+	if (ODR == MAG_ODR_0_625HZ)
+	{
+		reg1Config &= ~(7<<2);
+	}
+	else if (ODR == MAG_ODR_1_25HZ)
+	{
+		reg1Config &= ~(7<<2);
+		reg1Config |= (1<<2);
+	}
+	else if (ODR == MAG_ODR_2_5HZ)
+	{
+		reg1Config &= ~(7<<2);
+	    reg1Config |= (1<<3);
+	}
+	else if (ODR == MAG_ODR_5HZ)
+	{
+		reg1Config &= ~(7<<2);
+	    reg1Config |= (3<<2);
+	}
+	else if (ODR == MAG_ODR_10HZ)
+	{
+		reg1Config &= ~(7<<2);
+	    reg1Config |= (1<<4);
+	}
+	else if (ODR == MAG_ODR_20HZ)
+	{
+		reg1Config &= ~(7<<2);
+	    reg1Config |= (5<<2);
+	}
+	else if (ODR == MAG_ODR_40HZ)
+	{
+		reg1Config &= ~(7<<2);
+	    reg1Config |= (3<<3);
+	}
+	else
+	{
+		/*- ODR 80Hz  -*/
+		reg1Config |= (7<<2);
+	}
+
+	/*- X,Y and Z Axis Operational Mode Configuration  -*/
+	if (xyzOM == MAG_OM_LOW_POWER)
+	{
+		/*- X and Y Operational Mode -*/
+		reg1Config &= ~(3<<5);
+		/*- Z Operational Mode -*/
+		reg4Config &= ~(3<<2);
+	}
+	else if (xyzOM == MAG_OM_MEDIUM_POWER)
+	{
+		/*- X and Y Operational Mode -*/
+		reg1Config &= ~(3<<5);
+		reg1Config |= (1<<5);
+		/*- Z Operational Mode -*/
+		reg4Config &= ~(3<<2);
+		reg4Config |= (1<<2);
+
+	}
+	else if (xyzOM == MAG_OM_HIGH_POWER)
+	{
+		/*- X and Y Operational Mode -*/
+		reg1Config &= ~(3<<5);
+		reg1Config |= (1<<6);
+		/*- Z Operational Mode -*/
+		reg4Config &= ~(3<<2);
+		reg4Config |= (1<<3);
+
+	}
+	else
+	{
+		/*- Operational Mode is Ultra-high Performance Mode  -*/
+		/*- X and Y Operational Mode -*/
+		reg1Config |= (3<<5);
+		/*- Z Operational Mode -*/
+		reg4Config |= (3<<2);
+	}
+
+	/*- Full-Scale Configuration  -*/
+	if (FScale == MAG_FS_4G)
+	{
+		reg2Config &= ~(3<<5);
+	}
+	else if (FScale == MAG_FS_8G)
+	{
+		reg2Config &= ~(3<<5);
+		reg2Config |= (1<<5);
+	}
+	else if (FScale == MAG_FS_12G)
+	{
+		reg2Config &= ~(3<<5);
+		reg2Config |= (1<<6);
+	}
+	else
+	{
+		/*- FS: ±16 Gauss -*/
+		reg2Config |= (3<<5);
+	}
+
+	/*- Measurement Mode Configuration  -*/
+	if (measurementMode == MAG_CONTINUOUS_CONV)
+	{
+		reg3Config &= ~(3<<0);
+	}
+	else if (measurementMode == MAG_SINGLE_CONV)
+	{
+		reg3Config &= ~(3<<0);
+		reg3Config |= (1<<0);
+	}
+	else
+	{
+		/*- Power Down Mode -*/
+		reg3Config |= (3<<0);
+	}
+
+	/*- Configure registers with user settings  -*/
+
+	HAL_I2C_Mem_Write(pI2CHandle, MAG_ADDRESS, CTRL_REG_1, I2C_MEMADD_SIZE_8BIT, &reg1Config, 1, HAL_MAX_DELAY);
+	HAL_I2C_Mem_Write(pI2CHandle, MAG_ADDRESS, CTRL_REG_2, I2C_MEMADD_SIZE_8BIT, &reg2Config, 1, HAL_MAX_DELAY);
+	HAL_I2C_Mem_Write(pI2CHandle, MAG_ADDRESS, CTRL_REG_3, I2C_MEMADD_SIZE_8BIT, &reg3Config, 1, HAL_MAX_DELAY);
+	HAL_I2C_Mem_Write(pI2CHandle, MAG_ADDRESS, CTRL_REG_4, I2C_MEMADD_SIZE_8BIT, &reg4Config, 1, HAL_MAX_DELAY);
+	HAL_I2C_Mem_Write(pI2CHandle, MAG_ADDRESS, CTRL_REG_5, I2C_MEMADD_SIZE_8BIT, &reg5Config, 1, HAL_MAX_DELAY);
 
 }
 
@@ -115,7 +266,7 @@ void LIS3MDL_DeInit(I2C_HandleTypeDef *pI2CHandle)
 	uint8_t data = 0;
 	HAL_I2C_Mem_Read(pI2CHandle, MAG_ADDRESS, CTRL_REG_2, I2C_MEMADD_SIZE_8BIT, &data, 1, HAL_MAX_DELAY);
 	data |= (1<<2);
-	HAL_I2C_Mem_Write(&hi2c1,(0x1C << 1), 0x21, I2C_MEMADD_SIZE_8BIT, &data , 1, HAL_MAX_DELAY);
+	HAL_I2C_Mem_Write(pI2CHandle,(0x1C << 1), 0x21, I2C_MEMADD_SIZE_8BIT, &data , 1, HAL_MAX_DELAY);
 
 }
 
@@ -128,8 +279,8 @@ void LIS3MDL_DeInit(I2C_HandleTypeDef *pI2CHandle)
  * ------------------------------------------------------------------------------------------------------ */
 int16_t LIS3MDL_getMAG_X(I2C_HandleTypeDef *pI2CHandle)
 {
-	int8_t Out_L = 0;
-	int8_t Out_H = 0;
+	uint8_t Out_L = 0;
+	uint8_t Out_H = 0;
 	/*- Read register OUT_X_L -*/
 	HAL_I2C_Mem_Read(pI2CHandle, MAG_ADDRESS, OUT_X_L, I2C_MEMADD_SIZE_8BIT, &Out_L, 1, HAL_MAX_DELAY);
 	/*- Read register OUT_X_H -*/
@@ -147,8 +298,8 @@ int16_t LIS3MDL_getMAG_X(I2C_HandleTypeDef *pI2CHandle)
  * ------------------------------------------------------------------------------------------------------ */
 int16_t LIS3MDL_getMAG_Y(I2C_HandleTypeDef *pI2CHandle)
 {
-	int8_t Out_L = 0;
-	int8_t Out_H = 0;
+	uint8_t Out_L = 0;
+	uint8_t Out_H = 0;
 	/*- Read register OUT_Y_L -*/
 	HAL_I2C_Mem_Read(pI2CHandle, MAG_ADDRESS, OUT_Y_L, I2C_MEMADD_SIZE_8BIT, &Out_L, 1, HAL_MAX_DELAY);
 	/*- Read register OUT_Y_H -*/
@@ -166,8 +317,8 @@ int16_t LIS3MDL_getMAG_Y(I2C_HandleTypeDef *pI2CHandle)
  * ------------------------------------------------------------------------------------------------------ */
 int16_t LIS3MDL_getMAG_Z(I2C_HandleTypeDef *pI2CHandle)
 {
-	int8_t Out_L = 0;
-	int8_t Out_H = 0;
+	uint8_t Out_L = 0;
+	uint8_t Out_H = 0;
 	/*- Read register OUT_Z_L -*/
 	HAL_I2C_Mem_Read(pI2CHandle, MAG_ADDRESS, OUT_Z_L, I2C_MEMADD_SIZE_8BIT, &Out_L, 1, HAL_MAX_DELAY);
 	/*- Read register OUT_Z_H -*/
@@ -180,21 +331,19 @@ int16_t LIS3MDL_getMAG_Z(I2C_HandleTypeDef *pI2CHandle)
  * Name		:	LIS3MDL_getTemperature_C
  * Description	:	Returns temperature sensor's output
  * Parameter 1	:	Pointer to I2C Handle
- * Return Type	:	int16_t (16 bit signed value)
+ * Return Type	:	float
  * Note		:
  * ------------------------------------------------------------------------------------------------------ */
-int16_t LIS3MDL_getTemperature_C(I2C_HandleTypeDef *pI2CHandle)
+float LIS3MDL_getTemperature_C(I2C_HandleTypeDef *pI2CHandle)
 {
-	int8_t Out_L = 0;
-	int8_t Out_H = 0;
-	int16_t Temp_c = 0;
+	uint8_t Out_L = 0;
+	uint8_t Out_H = 0;
 	/*- Read register OUT_Y_L -*/
 	HAL_I2C_Mem_Read(pI2CHandle, MAG_ADDRESS, TEMP_OUT_L, I2C_MEMADD_SIZE_8BIT, &Out_L, 1, HAL_MAX_DELAY);
 	/*- Read register OUT_Y_H -*/
 	HAL_I2C_Mem_Read(pI2CHandle, MAG_ADDRESS, TEMP_OUT_H, I2C_MEMADD_SIZE_8BIT, &Out_H, 1, HAL_MAX_DELAY);
 
-	// configure offset and scaling
-	Temp_c = (((int16_t)(Out_H << 8) | Out_L) / 256) + 25;
-	return Temp_c;
+	// configure offset and scaling and return
+	return (((int16_t)(Out_H << 8) | Out_L) / 256.0f) + 25;
 
 }
